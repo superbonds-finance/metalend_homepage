e7235bacc0dfe25ceb1ef3b6e32b898adca031b1
sitanshu-kumar-vst-au4 <sitanshu4@gmail.com>
2022-04-05 11:56:24 +0530

    homepage changes

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -40,11 +40,7 @@ export default function Navbar(props: {
   <div className="flex md:flex-col">
     <div className="flex justify-between">
       <button
-        onClick={() =>
-          window.open(
-            "https://res.cloudinary.com/drr1rnoxf/image/upload/v1642360290/SB_Whitepaper-compressed_lafdtl.pdf"
-          )
-        }
+         
         className="hover:bg-green-100  text-white hover:text-black mr-2 border-2 z-40  rounded-md border-green-100 px-5 md:px-3 sm:px-2 py-1 inline-block ml-3"
       >
         <TextDoc transform="" className="" size="16px" weight="true">
