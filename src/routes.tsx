e6788e2390f6a84b5287c47a2ae2b8b0825088e6
Nad128668 <briannguyen0102@yahoo.co.uk>
2022-04-18 14:35:54 +0700

    remove admin access

--- src/routes.tsx
+++ src/routes.tsx
@@ -58,15 +58,15 @@ export function Routes() {
                 <Switch>
                   <Route exact path="/" component={() => <HomeView />} />
                   <Route exact path="/myaccount" children={<MyAccountView />} />
-                  <Route exact path="/pool-management" children={<PoolManagementView />} />
-                  <Route exact path="/platform-settings" children={<StakingManagementView />} />
+                  {/*<Route exact path="/pool-management" children={<PoolManagementView />} />
+                  <Route exact path="/platform-settings" children={<StakingManagementView />} />*/}
                   <Route exact path="/liquidity" children={<LiquidityView />} />
                   <Route exact path="/trade" children={<TradeView />} />
                   {/* <Route exact path="/trade-ui" children={<TradeViewUI />} /> */}
                   <Route exact path="/stake" children={<StakeView />} />
                   <Route exact path="/redeem/:trade_account" children={<RedeemView />} />
                   <Route exact path="/governance" children={<GovernanceView />} />
-                  <Route exact path="/farming_rewards" children={<FarmingRewardsView />} />
+                  {/*<Route exact path="/farming_rewards" children={<FarmingRewardsView />} />*/}
                   <Route exact path="/platform" children={<PlatformStatsView />} />
                 </Switch>
               </AppLayout>
